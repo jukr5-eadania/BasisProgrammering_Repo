703c5057d355a564316e0ea46b7f1f46cc374c7d
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-23 14:26:17 +0200

    Gul Øvelse færdig
    Tilføj While loop?

--- Øvelse_4/Program.cs
+++ Øvelse_4/Program.cs
@@ -10,6 +10,64 @@ namespace Øvelse_4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What operation do you want to use? (Addition, Subtraction, Multiplication or Division)");
+
+            string operation = Console.ReadLine();
+            int firstNumber;
+            int secondNumber;
+            int inputFirstNumber;
+            int inputSecondNumber;
+            int sum;
+            
+            switch (operation)
+            {
+                case "Addition":
+                    Console.WriteLine("What is the first number you want to use?");
+                    firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("What is the second number you want to use?");
+                    secondNumber = inputSecondNumber = Convert.ToInt32(Console.ReadLine());
+                    sum = firstNumber += secondNumber;
+                    Console.WriteLine(inputFirstNumber + " plus " + inputSecondNumber + " is equal to " + sum);
+                    break;
+
+                case "Subtraction":
+                    Console.WriteLine("What is the first number you want to use?");
+                    firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("What is the second number you want to use?");
+                    secondNumber = inputSecondNumber = Convert.ToInt32(Console.ReadLine());
+                    sum = firstNumber -= secondNumber;
+                    Console.WriteLine(inputFirstNumber + " minus " + inputSecondNumber + " is equal to " + sum);
+                    break;
+
+                case "Multiplication":
+                    Console.WriteLine("What is the first number you want to use?");
+                    firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("What is the second number you want to use?");
+                    secondNumber = inputSecondNumber = Convert.ToInt32(Console.ReadLine());
+                    sum = firstNumber *= secondNumber;
+                    Console.WriteLine(inputFirstNumber + " times " + inputSecondNumber + " is equal to " + sum);
+                    break;
+
+
+                case "Division":
+                    Console.WriteLine("What is the first number you want to use?");
+                    firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("What is the second number you want to use?");
+                    secondNumber = inputSecondNumber = Convert.ToInt32(Console.ReadLine());
+                    sum = firstNumber /= secondNumber;
+                    Console.WriteLine(inputFirstNumber + " divided " + inputSecondNumber + " is equal to " + sum);
+                    break;
+
+                default:
+                    Console.WriteLine("You did not write the correct thing");
+                    Console.WriteLine();
+                    Console.ReadLine();
+                    break;
+                    
+            }
+
+            //Pauses the program
+            Console.ReadLine();
         }
     }
 }
