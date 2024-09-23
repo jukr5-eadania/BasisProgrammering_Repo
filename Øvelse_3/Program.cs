b14af49b031e2dd1fe619f1529fe87007c1f260e
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-23 13:10:59 +0200

    Grøn og gul øvelse færdig

--- Øvelse_3/Program.cs
+++ Øvelse_3/Program.cs
@@ -10,6 +10,30 @@ namespace Øvelse_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("What is your phone number");
+            int phoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What country do you live in?");
+            string country = Console.ReadLine();
+
+            Console.WriteLine("What city do you live in?");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("What is your city's postal code");
+            int postalCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your adress?");
+            string adress = Console.ReadLine();
+
+            Console.WriteLine("Name: " + name + Environment.NewLine + "Age: " + age + Environment.NewLine + "Phone Number: " + phoneNumber + Environment.NewLine + "Country " + country + Environment.NewLine + "City: " + city + Environment.NewLine + "Postal Code: " + postalCode + Environment.NewLine + "Adress: " + adress);
+
+            Console.ReadLine();         
         }
     }
 }
