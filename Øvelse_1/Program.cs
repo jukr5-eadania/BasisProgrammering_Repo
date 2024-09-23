7f512a304b0200a02997a0313bb4ca75aee146f0
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-23 12:33:27 +0200

    Initial Commit

---
+++ Øvelse_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This line writes "Hello World" to the console
+            Console.WriteLine("Hello World");
+
+            //Writes a message to the user
+            Console.WriteLine("Press any key to close the program");
+
+            //Writes a message without a new line for the next text
+            Console.Write("Console.Write writes text that does not make a new line for the next text.");
+
+            //Writes a message with a new line for the next text
+            Console.WriteLine(" Console.WriteLine writes text that adds a line after the text for the new text");
+
+            //Colors the foreground magenta and the background white and outputs a line in that color
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("This texts color is magenta while the background is white");
+
+            //Makes the console say a beep sound when opening
+            Console.Beep();
+
+            //This line waits for user input (pauses the program)
+            //If we don't do this the program will close right away
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Øvelse_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variable cki
+            ConsoleKeyInfo cki;
+
+            //Writes the action message
+            Console.WriteLine("Please enter your name");
+
+            //Reads the users input
+            string name = Console.ReadLine();
+
+            //Writes the welcome message with the user input
+            Console.WriteLine($"Welcome {name}");
+
+            //Assigning variable to cki
+            //Console.ReadKey reads when a key is pressed
+            cki = Console.ReadKey();
+
+            //Writes what key is pressed
+            Console.Write(" --- You Pressed ");
+            Console.WriteLine(cki.Key.ToString());
+
+
+
+            //Section below writes user input without variables
+            
+            //Writes action message
+            Console.WriteLine("Please enter your name");
+            
+            //Writes welcome message based on user input
+            Console.WriteLine("Welcome " + Console.ReadLine());
+
+            //Pauses the program
+            Console.ReadLine();
+        }
+    }
+}
