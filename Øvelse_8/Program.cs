c24d4cfa4d2d6bf89e6469460b2a496ca8bbbd32
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-24 14:12:47 +0200

    Grøn øvelse done

--- Øvelse_8/Program.cs
+++ Øvelse_8/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Øvelse_8
@@ -10,7 +11,35 @@ namespace Øvelse_8
     {
         static void Main(string[] args)
         {
+            //Assigns variables
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+            int milliseconds = 500;
 
+            //While loop that counts to 10
+            while (count1 < 11)
+            {
+                Console.WriteLine("While loop: " + count1);
+                count1 += 1;
+                Thread.Sleep(milliseconds);
+            }
+
+            //Do...While loop that counts to 10
+            do
+            {
+                Console.WriteLine("Do...While loop: " + count2);
+                count2 += 1;
+                Thread.Sleep(milliseconds);
+            } while (count2 < 11);
+
+            //For loop that counts to 10
+            for (count3 = 0; count3 < 11; count3++)
+            {
+                Console.WriteLine("For loop: " + count3);
+                Thread.Sleep(milliseconds);
+
+            }
 
         }
     }
