6097426baeb375bcdd9e7d42e1997bb4d6112a50
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-24 09:48:13 +0200

    Initial commit

---
+++ Obl_Øvelse_Variabler&DataTyper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obl_Øvelse_Variabler_DataTyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            byte guessNumber;
+            byte randomNumber;
+
+            Console.WriteLine("WELCOME");
+            Console.WriteLine("TO");
+            Console.WriteLine("THE");
+            Console.WriteLine("GAMESHOW!");
+            Console.WriteLine("Contestant! What is your name?");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine(name + " huh? What a... name...");
+            Console.WriteLine("Anyways, " + name + " Guess a number between 1 and 100 and you will be the winner of a big big price!");
+
+            guessNumber = 
+
+            Console.ReadLine();
+        }
+    }
+}
