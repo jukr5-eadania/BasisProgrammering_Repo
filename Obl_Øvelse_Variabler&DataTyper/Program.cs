f961f6beb69616c367335161605971dc443510e0
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-24 10:38:08 +0200

    Program works

--- Obl_Øvelse_Variabler&DataTyper/Program.cs
+++ Obl_Øvelse_Variabler&DataTyper/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Obl_Øvelse_Variabler_DataTyper
@@ -11,23 +13,62 @@ namespace Obl_Øvelse_Variabler_DataTyper
         static void Main(string[] args)
         {
             string name;
-            byte guessNumber;
-            byte randomNumber;
+            int guessNumber;
+            int difference;
+            int milliseconds = 2000;
+
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 101);
+
+            Console.WriteLine(randomNumber);
 
             Console.WriteLine("WELCOME");
+            Thread.Sleep(milliseconds);
             Console.WriteLine("TO");
+            Thread.Sleep(milliseconds);
             Console.WriteLine("THE");
+            Thread.Sleep(milliseconds);
             Console.WriteLine("GAMESHOW!");
+            Thread.Sleep(milliseconds);
             Console.WriteLine("Contestant! What is your name?");
 
             name = Console.ReadLine();
 
-            Console.WriteLine(name + " huh? What a... name...");
+            Console.WriteLine(name + " huh? What a wierd name...");
+            Thread.Sleep(milliseconds);
             Console.WriteLine("Anyways, " + name + " Guess a number between 1 and 100 and you will be the winner of a big big price!");
 
-            guessNumber = 
+            guessNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
+            difference = Math.Abs(randomNumber - guessNumber);
+
+            if (difference == 0)
+            {
+                Console.WriteLine("Congratulations!!! " + name + " You won the big big price of...");
+                Thread.Sleep(milliseconds);
+                Console.WriteLine("NOTHING!!!");
+                Thread.Sleep(milliseconds);
+                Console.WriteLine("Now, get out... Use the enter key to leave");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("and the number is is... ");
+                Thread.Sleep(milliseconds);
+                Console.WriteLine(randomNumber);
+                Thread.Sleep(milliseconds);
+                Console.WriteLine("Ooooh, Sorry. You didn't guess correct...");
+                Thread.Sleep(milliseconds);
+                Console.WriteLine("You were only... " + difference + " off from the actual number");
+                Thread.Sleep(milliseconds);
+                Console.WriteLine("TOO BAD");
+                Console.WriteLine("You know what happens next...");
+
+                Console.WriteLine("Press enter to RUN!");
+                Console.ReadLine();
+            }
+            
         }
     }
 }
