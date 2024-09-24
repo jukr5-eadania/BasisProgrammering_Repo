e0545a88987dabe0d3f0b4bb604cc231081e235f
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-24 09:27:22 +0200

    comments added

--- Øvelse_4/Program.cs
+++ Øvelse_4/Program.cs
@@ -10,8 +10,10 @@ namespace Øvelse_4
     {
         static void Main(string[] args)
         {
+            //Asks user a question
             Console.WriteLine("What operation do you want to use? (Addition, Subtraction, Multiplication or Division)");
 
+            //Defining variables
             string operation = Console.ReadLine();
             int firstNumber;
             int secondNumber;
@@ -19,8 +21,10 @@ namespace Øvelse_4
             int inputSecondNumber;
             int sum;
             
+            //Calling switch operation which asks the user what type of operation they want to use
             switch (operation)
             {
+                //Case for addition, asking the user for two numbers and adding them toghether while showing the original numbers and results
                 case "Addition":
                     Console.WriteLine("What is the first number you want to use?");
                     firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
@@ -30,6 +34,7 @@ namespace Øvelse_4
                     Console.WriteLine(inputFirstNumber + " plus " + inputSecondNumber + " is equal to " + sum);
                     break;
 
+                //Case for Subtraction, asking the user for two numbers and subtracting them from each other while showing the original numbers and results
                 case "Subtraction":
                     Console.WriteLine("What is the first number you want to use?");
                     firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
@@ -39,6 +44,7 @@ namespace Øvelse_4
                     Console.WriteLine(inputFirstNumber + " minus " + inputSecondNumber + " is equal to " + sum);
                     break;
 
+                //Case for multiplication, asking the user for two numbers and mimltyplying them while showing the original numbers and results
                 case "Multiplication":
                     Console.WriteLine("What is the first number you want to use?");
                     firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
@@ -48,7 +54,7 @@ namespace Øvelse_4
                     Console.WriteLine(inputFirstNumber + " times " + inputSecondNumber + " is equal to " + sum);
                     break;
 
-
+                //Case for Division, asking the user for two numbers and dividing them while showing the original numbers and results
                 case "Division":
                     Console.WriteLine("What is the first number you want to use?");
                     firstNumber = inputFirstNumber = Convert.ToInt32(Console.ReadLine());
@@ -58,6 +64,7 @@ namespace Øvelse_4
                     Console.WriteLine(inputFirstNumber + " divided " + inputSecondNumber + " is equal to " + sum);
                     break;
 
+                //If the user does not the correct case, they will be asked again
                 default:
                     Console.WriteLine("You did not write the correct thing");
                     Console.WriteLine();
