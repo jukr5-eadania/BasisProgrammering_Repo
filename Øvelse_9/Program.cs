36733bad3e266f3ce092848b1ed6b18246c470fe
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-25 11:03:58 +0200

    Enus done

--- Øvelse_9/Program.cs
+++ Øvelse_9/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Øvelse_9
 {
+    enum Direction
+    {
+        North, South, West, East
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -48,7 +52,7 @@ namespace Øvelse_9
             */
 
             /*
-            //Casting int to byte (Overflow)
+            //Casting int to byte: (Overflow)
             //Assign variables
             Byte castingByte;
             int castingInt = 300;
@@ -67,15 +71,62 @@ namespace Øvelse_9
             }
             */
 
-            //Casting float to int
+            /*
+            //Casting float to int:
             //Assign variables
             float myfloat = 2322.223f;
             int myInt;
 
+            //Casting float to int
             myInt = (int)myfloat;
 
+            //Writes result and pauses program
             Console.WriteLine(myInt);
             Console.ReadLine();
+            */
+
+
+            //Enums:
+            //Assigning variables
+            Direction myDirection = Direction.North;
+            int playerDirection;
+
+            //Writes assigned enum and pauses program
+            Console.WriteLine(myDirection);
+            Console.ReadLine();
+
+            //Asks player for direction input
+            Console.WriteLine("What direction do you want to move? (1 = North, 2 = East, 3 = South, 4 = West");
+            playerDirection = int.Parse(Console.ReadLine());
+
+            //Uses different switch cases based on player input
+            switch (playerDirection)
+            {
+                //Moves player North
+                case 1:
+                    myDirection = Direction.North;
+                    break;
+
+                //Moves player East
+                case 2:
+                    myDirection = Direction.East;
+                    break;
+
+                //Moves player South
+                case 3:
+                    myDirection = Direction.South;
+                    break;
+
+                //Moves player West
+                case 4:
+                    myDirection = Direction.West;
+                    break;
+            }
+
+            //Writes player direction and pauses program
+            Console.WriteLine("You went " + myDirection);
+            Console.ReadLine();
+
 
         }
     }
