a6d065fdbe960967dce6ea86fdd5959a3a6de856
jukr5-eadania <jukr5@student.eadania.dk>
2024-09-26 10:18:37 +0200

    Grøn opgave færdig

--- Øvelse_12/Program.cs
+++ Øvelse_12/Program.cs
@@ -10,6 +10,50 @@ namespace Øvelse_12
     {
         static void Main(string[] args)
         {
+            //Defines array
+            int[] numberArray = new int[7] { 12, 33, 532, 145, 1, 4565, 1 };
+
+            //Writes the firth element in numberArray (0 always comes first in arrays)
+            Console.WriteLine(numberArray[4]);
+
+            //Changes the 3rd element in numberArray
+            numberArray[2] = 36562;
+            
+            //Writes all values in numberarray using a for loop
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                Console.WriteLine(numberArray[i]);
+            }
+
+            //Writes all values in numberarray using a foreach loop
+            foreach (int number in numberArray)
+            {
+                Console.WriteLine(number.ToString());
+            }
+
+            //Defines Array
+            string[] stringArray = new string[10] {"Emilie", "Mads", "Malte", "Jeppe", "Irene", "Thomas", "Echo", "Emma", "Tobias", "Stephanie"};
+
+            //Writes the firth element in stringArray (0 always comes first in arrays)
+            Console.WriteLine(stringArray[4]);
+
+            //Changes the 3rd element in stringArray
+            stringArray[2] = "Mario";
+
+            //Writes all values in stringArray using a for loop
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i]);
+            }
+
+            //Writes all values in stringArray using a foreach loop
+            foreach (string name in stringArray)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Pauses the program
+            Console.ReadLine();
         }
     }
 }
